12c8f6a8928453c08f03c1523f711a8dd84d44b3
Reece Morgan <morgaree@flip2.engr.oregonstate.edu>
2023-04-13 21:00:55 -0700

    set up terminal input and started figlet

--- asciiArt.js
+++ asciiArt.js
@@ -1,1 +1,13 @@
-console.log()
+
+
+//process.argv returns an array containing the command-line arguments passed when the node.js process was launched
+argv.forEach((val, index) => {
+	console.log('${index}: ${val}');
+});
+
+figlet.text(
+      input,
+      {
+      	
+      }
+      )
