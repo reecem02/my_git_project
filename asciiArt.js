046df4898a34b5e456bab8909f8f4ba9fa516efa
Reece Morgan <morgaree@flip1.engr.oregonstate.edu>
2023-04-14 14:17:08 -0700

    finished incorporating figlet and got it to print ascii for small termal string inputs

--- asciiArt.js
+++ asciiArt.js
@@ -1,13 +1,22 @@
-
+/* Name: Reece Morgan
+/* Date: 4/14/23
+/* Assignment 1
+*/
 
 //process.argv returns an array containing the command-line arguments passed when the node.js process was launched
-argv.forEach((val, index) => {
-	console.log('${index}: ${val}');
-});
+// argv.forEach((val, index) => {
+// 	console.log('${index}: ${val}');
+// });
 
-figlet.text(
-      input,
-      {
-      	
+
+var figlet = require("figlet");
+
+figlet(process.argv[2], function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
       }
-      )
+      console.log(data)
+    });
+
